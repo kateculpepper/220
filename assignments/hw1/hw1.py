8a9d2ed098a6d6359acd0719a4f682dea1fac798
kateculpepper <culpepperke@g.cofc.edu>
2022-01-21 18:08:02 -0500

    completed hw1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,35 +1,53 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Kate Culpepper>
+<HomeWork1>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <This assignment runs a series of different programs that allow you to find
+area, volume, shooting percentages for athletes, prices of a coffee sale and a conversion program >
 
 Certification of Authenticity:
-<include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+
+I certify that this assignment is my own work, but I discussed it with: <Kate Culpepper>
 """
 
 
 def calc_rec_area():
-    pass
+    length = eval(input("Enter the Length:"))
+    width = eval(input("Enter the width"))
+    area = length * width
+    print("Area= ", area)
 
 
 def calc_volume():
-    pass
+    length = eval(input("Enter the length"))
+    width = eval(input("Enter the width"))
+    height = eval(input("Enter the height"))
+    volume = length * width * height
+    print("volume =", volume)
 
 
 def shooting_percentage():
-    pass
+    shots = eval(input("Enter the Player's total shots:"))
+    shotsmade = eval(input("Enter how many shots the player made:"))
+    percentage = shotsmade / shots * 100
+    print("Shooting percentage:", percentage, "%")
 
 
 def coffee():
-    pass
+    pounds = eval(input("How many pounds of coffee would like?"))
+    total = pounds * 11.36 + 1.50
+    print("You're total is :", total)
 
 
 def kilometers_to_miles():
-    pass
+    kilometers = eval(input("How many kilometers did you travel?"))
+    miles = kilometers * .62137
+    print("That's", miles, "miles!")
 
 
 if __name__ == '__main__':
-    pass
+    calc_rec_area()
+    calc_volume()
+    shooting_percentage()
+    coffee()
+    kilometers_to_miles()
