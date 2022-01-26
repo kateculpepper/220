cda79472234faa1e8a413d8a878388bf662d681a
kateculpepper <culpepperke@g.cofc.edu>
2022-01-25 21:37:58 -0500

    Completed lab 2

--- labs/lab2/lab2.py
+++ labs/lab2/lab2.py
@@ -8,8 +8,9 @@ Certification of Authenticity:
 
 I certify that this assignment is my own work <Kate Culpepper>
 """
-def means():
 
+
+def means():
     user_range = eval(input("Enter the number of values to be entered: "))
 
     rms_acc = 0
@@ -20,16 +21,16 @@ def means():
         values = eval(input("Enter value:"))
         square_values = values ** 2
         rms_acc = rms_acc + square_values
-        harmonic_mean_acc = harmonic_mean_acc + 1/values
+        harmonic_mean_acc = harmonic_mean_acc + 1 / values
         geometric_mean_acc = geometric_mean_acc * values
 
-    rms = (rms_acc/user_range) ** .5
+    rms = (rms_acc / user_range) ** .5
     print("The root mean square is :", rms)
 
-    hm = (user_range/harmonic_mean_acc)
+    hm = (user_range / harmonic_mean_acc)
     print("The Harmonic mean is:", hm)
 
-    gm = (geometric_mean_acc ** (1/user_range))
+    gm = (geometric_mean_acc ** (1 / user_range))
     print("The geometric mean is:", gm)
 
 
