685c8cf76b0d932add49b642e19459a135219b52
kateculpepper <culpepperke@g.cofc.edu>
2022-04-01 19:10:13 -0400

    lab 10

--- assignments/hw10/face.py
+++ assignments/hw10/face.py
@@ -11,32 +11,50 @@ class Face:
         self.head = Circle(center, size)
         self.head.draw(window)
         self.left_eye = Circle(center, eye_size)
-        self.left_eye.move(-eye_off, -eye_off)
+        self.left_eye.move(-eye_off, eye_off)
         self.right_eye = Circle(center, eye_size)
-        self.right_eye.move(eye_off, -eye_off)
+        self.right_eye.move(eye_off, eye_off)
         self.left_eye.draw(window)
         self.right_eye.draw(window)
         point_1 = center.clone()
-        point_1.move(-mouth_size / 2, mouth_off)
+        point_1.move(-mouth_size / 2, -mouth_off)
         point_2 = center.clone()
-        point_2.move(mouth_size / 2, mouth_off)
+        point_2.move(mouth_size / 2, -mouth_off)
         self.mouth = Line(point_1, point_2)
         self.mouth.draw(window)
 
-    def smile(self, center, size):
+    def smile(self,window, center, size):
         self.mouth.undraw()
         mouth_size = 0.8 * size
         mouth_off = size / 2.0
         point_1 = center.clone()
-        point_1.move(-mouth_size / 2, mouth_off)
+        point_1.move(-mouth_size / 2, -mouth_off)
         point_2 = center.clone()
-        point_2.move(mouth_size / 2, mouth_off)
-        point_3 = center.clone
-        point3.move(mouth_size)
-        smile = Polygon()
+        point_2.move(mouth_size / 2, -mouth_off)
+        point_3 = center.clone()
+        point_3.move(0, -mouth_size)
+        smile = Polygon(point_1, point_2, point_3)
+        smile.draw(window)
 
-    def shock(self):
-        pass
+    def shock(self, window, center, size):
+        self.mouth.undraw()
+        mouth_size = .15 * size
+        mouth_off = size / 2.0
+        point_1 = center.clone()
+        point_1.move(0, -mouth_off )
+        mouth = Circle(point_1,  mouth_size)
+        mouth.draw(window)
 
-    def wink(self):
-        pass
+    def wink(self, window, center, size):
+        self.left_eye.undraw()
+        eye_size = 0.15 * size
+        eye_off = size / 3.0
+        point_1 = center.clone()
+        point_1.move(-eye_off/2 , eye_off)
+        point_2 = center.clone()
+        point_2.move(-eye_off - eye_size, eye_off)
+        self.left_eye = Line(point_1, point_2)
+        self.left_eye.draw(window)
+        self.smile(window, center, size)
+
+
--- assignments/hw10/hw10.py
+++ assignments/hw10/hw10.py
@@ -2,7 +2,7 @@ from graphics import *
 from face import Face
 
 
-# f = f n-1 + fn-2
+f = f n-1 + fn-2
 
 def fibonacci(n):
     if n < 1:
@@ -13,7 +13,7 @@ def fibonacci(n):
         return 0
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
-# am i allowed to use recursion,
+
 
 def double_investment(principle, rate):
     A = principle * (1 + rate)
@@ -44,28 +44,64 @@ def syracuse(n):
     pass
 
 
+def is_prime(n):
+    i = 2
+    while i < n:
+        if n % i == 0:
+            return False
+        else:
+            i = i + 1
+    return True
+
+
 def goldbach(n):  # output: [prime, prime]
 
-gold_list: []
-    while n > 2:
-        if n % 2 == 0:
-            return True
-        else:
-            return None
-pass
+    if n % 2 != 0:
+        return None
+
+    gold_list = []
+
+    num_1 = 2
+    num_2 = 2
+    if num_1 + num_2 == n:
+        gold_list.append(num_1)
+        gold_list.append(num_2)
+        return gold_list
+    acc = 3
+    num_1 = 3
+    num_2 = 3
+    while num_1 + num_2 < n :
+
+        while num_1 + num_2 < n:
+            if num_1 + num_2 == n:
+                break
+            if is_prime(num_2 + 2):
+                num_2 = num_2 + 2
+            else:
+                num_2 = num_2 + 4
+        num_2 = num_1
+    if num_1 + num_2 == n:
+        gold_list.append(num_1)
+        gold_list.append(num_2)
+        return gold_list
+    else:
+        return None
+
 
 def main():
-    width_px = 700
-    height_px = 700
+    width_px = 900
+    height_px = 900
     window = GraphWin("test", width_px, height_px)
     width = 10
     height = 10
     window.setCoords(0, 0, width, height)
 
-    Face(window, Point(5, 5), 4)
+    face = Face(window, Point(5, 5), 4)
+    face.wink(window, Point(5, 5), 4)
     window.getMouse()
     window.close()
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    print(is_prime(9))
